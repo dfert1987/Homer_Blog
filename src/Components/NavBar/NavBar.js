5f99434624c14f280e10db563d197b16847d1b97
David Fertitta <davidfertitta@Davids-MacBook-Pro.local>
2021-02-20 00:20:27 -0700

    responsive

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,86 @@
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import {Button} from "../Button/Button";
+import "./NavBar.css";
+
+function NavBar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-container">
+            <Link to="/" className="navbar-logo">
+              HOMER <i className="logo" />
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+              <i className={click ? " fas fa-times" : "fas fa-bars"} />
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/bears"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Bears
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/bulls"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Bulls
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/cubs"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Cubs
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/sign-up"
+                  className="nav-links-mobile"
+                  onClick={closeMobileMenu}
+                >
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+            {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
+            {/*  */}
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;
