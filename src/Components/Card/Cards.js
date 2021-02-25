8b8f73f639df91fbde82f68ac1101de4bf46c0c6
David Fertitta <davidfertitta@Davids-MacBook-Pro.local>
2021-02-25 00:04:57 -0700

    about start

--- src/Components/Card/Cards.js
+++ src/Components/Card/Cards.js
@@ -11,13 +11,13 @@ function Cards() {
           <ul className="cards-items">
             <CardItem
               src="https://i.imgur.com/8arrSvM.jpg"
-              text="Undeniable: Zach Lavine is an All-Star"
+              text="Don't look now, but Zach Lavine officially fucks."
               label="Bulls"
               path="/bulls"
             />
             <CardItem
               src="https://i.imgur.com/EmVgHk2.jpg"
-              text="Life After Mitch: Why is it so hard for some to let go?"
+              text="The curious case of Mitch Trubisky apologists."
               label="Bears"
               path="/bears"
             />
@@ -25,19 +25,19 @@ function Cards() {
           <ul className="cards-items">
             <CardItem
               src="https://i.imgur.com/ZZ5dLJU.jpg"
-              text="How Did We Get Here? The Suddenly Bleak State of the Cubs"
+              text="How Did We Get Here? The Suddenly Bleak State of the Cubs."
               label="Cubs"
               path="/cubs"
             />
             <CardItem
               src="https://i.imgur.com/MwgKmUM.jpg"
-              text="Pace and Nagy: An Honest Evaluation"
+              text="Pace and Nagy: So, how much can we blame these guys?"
               label="Bears"
               path="/bears"
             />
             <CardItem
               src="https://i.imgur.com/Y2Eorvu.jpg"
-              text="Thad Young: An Ode to the Journeyman"
+              text="Thad Young: An Ode to the NBA Journeyman."
               label="Bulls"
               path="/bulls"
             />
--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -2,5 +2,11 @@ import React from "react";
 import "../../App.css";
 
 export default function About() {
-  return <h1 className="about">WELCOME TO HOMER</h1>;
+  return (
+    <div className="about-container">
+      <h1 className="about-header">WELCOME TO HOMER</h1>
+      <h3 className="about-subheader">A Safe Place for Diehards</h3>
+      <img classname="about-image" alt="bears fan" src="https://i.imgur.com/LWoVpZv.jpg" />
+    </div>
+  );
 }
