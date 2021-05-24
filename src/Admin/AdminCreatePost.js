ab810accd3b3dea48693f8478cce6d2bbfcc72b5
David Fertitta <davidfertitta@Davids-MacBook-Pro-2.local>
2021-05-24 07:17:18 -0600

    saving quill with html

--- src/Admin/AdminCreatePost.js
+++ src/Admin/AdminCreatePost.js
@@ -1,56 +1,75 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback} from 'react';
 import './CreatePost.css';
 import ReactQuill from 'react-quill';
 
 export default function AdminCreatePost() {
-  const [body, setBody] = useState('');
+  const defaultBlog = {
+    title: '',
+    subtitle: '',
+    mainImage: '',
+    secondImage: '',
+    thirdImage: '',
+    body: '',
+    thumbsUp: 0,
+    thumbsDown: 0,
+    category: '',
+    authoer: '',
+  };
+  const [content, setContent] = useState('');
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+  const [blog, setBlog] = useState(defaultBlog);
 
-  const handleBody = (e) => {
-    console.log(e);
-    setBody(e);
+  const postData = {
+    body: content,
+    title: title,
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    fetch('http://localhost:3000/blogs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postData),
+    });
+    setContent('');
   };
 
   return (
     <div className='container'>
       <div className='main'>
         <h1 className='add-title'>Add A Post</h1>
-        <ReactQuill
-          placeholder="Start Bloggin'..."
-          modules={AdminCreatePost.modules}
-          formats={AdminCreatePost.formats}
-          onChange={handleBody}
-          value={body}
-        />
+        <form method='post' id='identifier'>
+          <label className='title-label'>Title:</label>
+          <input
+            className='title-input'
+            type='text'
+            placeholder='Some Title..'
+            value={title}
+            name='title'
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label className='subtitle-label'>Subtitle:</label>
+          <input
+            className='subtitle-input'
+            type='text'
+            placeholder='Some Subtitle..'
+            value={subtitle}
+            name='subtitle'
+            onChange={(e) => setSubtitle(e.target.value)}
+          />
+          <ReactQuill
+            placeholder="Start Bloggin'..."
+            modules={AdminCreatePost.modules}
+            formats={AdminCreatePost.formats}
+            onChange={setContent}
+            value={content}
+          />
+          <input type='submit' value='Add Post' onSubmit={onSubmit} />
+        </form>
       </div>
     </div>
   );
 }
-
-AdminCreatePost.modules = {
-  toolbar: [
-    [{header: '1'}, {header: '2'}, {header: [3, 4, 5, 6]}, {font: []}],
-    [{size: []}],
-    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-    [{list: 'ordered'}, {list: 'bullet'}],
-    ['link', 'image', 'video'],
-    ['clean'],
-    ['code-block'],
-  ],
-};
-
-AdminCreatePost.formats = [
-  'header',
-  'font',
-  'size',
-  'bold',
-  'italic',
-  'underline',
-  'strike',
-  'blockquote',
-  'list',
-  'bullet',
-  'link',
-  'image',
-  'video',
-  'code-block',
-];
