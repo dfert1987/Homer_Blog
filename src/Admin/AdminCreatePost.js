4a34264e6630a9a5016218ffa37f4f0a16c19afb
David Fertitta <davidfertitta@Davids-MacBook-Pro-2.local>
2021-05-24 22:25:48 -0600

    add a blog page edits

--- src/Admin/AdminCreatePost.js
+++ src/Admin/AdminCreatePost.js
@@ -1,29 +1,21 @@
 import React, {useState, useCallback} from 'react';
-import './CreatePost.css';
 import ReactQuill from 'react-quill';
 
+import './CreatePost.css';
+
 export default function AdminCreatePost() {
-  const defaultBlog = {
-    title: '',
-    subtitle: '',
-    mainImage: '',
-    secondImage: '',
-    thirdImage: '',
-    body: '',
-    thumbsUp: 0,
-    thumbsDown: 0,
-    category: '',
-    authoer: '',
-  };
   const [content, setContent] = useState('');
   const [title, setTitle] = useState('');
   const [subtitle, setSubtitle] = useState('');
-  const [blog, setBlog] = useState(defaultBlog);
+  const [category, setCategory] = useState('');
 
   const postData = {
     body: content,
     title: title,
+    category: category,
   };
+
+  console.log(content);
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -41,35 +33,82 @@ export default function AdminCreatePost() {
     <div className='container'>
       <div className='main'>
         <h1 className='add-title'>Add A Post</h1>
-        <form method='post' id='identifier'>
-          <label className='title-label'>Title:</label>
-          <input
-            className='title-input'
-            type='text'
-            placeholder='Some Title..'
-            value={title}
-            name='title'
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <label className='subtitle-label'>Subtitle:</label>
-          <input
-            className='subtitle-input'
-            type='text'
-            placeholder='Some Subtitle..'
-            value={subtitle}
-            name='subtitle'
-            onChange={(e) => setSubtitle(e.target.value)}
-          />
+        <form method='post' id='identifier' className='add-post-form'>
+          <div className='above-blog-container'>
+            <input
+              className='input'
+              type='text'
+              placeholder='Some Title..'
+              value={title}
+              name='title'
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <input
+              className='input'
+              type='text'
+              placeholder='Some Subtitle..'
+              value={subtitle}
+              name='subtitle'
+              onChange={(e) => setSubtitle(e.target.value)}
+            />
+            <select
+              className='category-input'
+              placeholder='Some Category..'
+              value={category}
+              name='category'
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value={null}>Select a Category:</option>
+              <option value='bulls'>Bulls</option>
+              <option value='bears'>Bears</option>
+              <option value='cubs'>Cubs</option>
+            </select>
+          </div>
           <ReactQuill
             placeholder="Start Bloggin'..."
             modules={AdminCreatePost.modules}
             formats={AdminCreatePost.formats}
             onChange={setContent}
             value={content}
+            className='quill'
           />
-          <input type='submit' value='Add Post' onSubmit={onSubmit} />
+          <input
+            className='submit-button'
+            type='submit'
+            value='Add Post'
+            onSubmit={onSubmit}
+          />
         </form>
       </div>
     </div>
   );
 }
+
+AdminCreatePost.modules = {
+  toolbar: [
+    [{header: '1'}, {header: '2'}, {header: [3, 4, 5, 6]}, {font: []}],
+    [{size: []}],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{list: 'ordered'}, {list: 'bullet'}],
+    ['link', 'image', 'video'],
+    ['clean'],
+    ['code-block'],
+  ],
+};
+
+AdminCreatePost.formats = [
+  'header',
+  'font',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'link',
+  'image',
+  'video',
+  'code-block'
+];
