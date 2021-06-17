89b5b0e9003ceeeaf1bb68ec469fd9c807b3cbdf
David Fertitta <davidfertitta@Davids-MBP-2.lan>
2021-06-16 23:30:55 -0600

    login/signup toggle set up

--- src/Components/LoginOptions/LoginForm.js
+++ src/Components/LoginOptions/LoginForm.js
@@ -8,8 +8,18 @@ export default function LoginForm({id, setSelectedForm}) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
+  const [showLogin, setShowLogin] = useState(true);
 
   const onSubmit = (event) => {
+    if (showLogin) {
+      loginUser();
+      event.preventDefault();
+    }
+    createUser();
+    event.preventDefault();
+  };
+
+  const loginUser = () => {
     const user = {
       username: username,
       password: password,
@@ -20,11 +30,10 @@ export default function LoginForm({id, setSelectedForm}) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(user),
-    })
+    });
     setUsername('');
     setPassword('');
     history.push('/');
-    event.preventDefault();
     return;
   };
 
@@ -40,20 +49,24 @@ export default function LoginForm({id, setSelectedForm}) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(newUser),
-    })
+    });
     setUsername('');
     setPassword('');
     setEmail('');
     history.push('/');
     return;
-  }
+  };
 
   const loginFormId = `${id}-login`;
 
   return (
     <div id={loginFormId} className='login-wrapper'>
       <div className='login-form-wrapper'>
-        <h3 className='login-form-title'>Sign In</h3>
+        {showLogin ? (
+          <h3 className='login-form-title'>Sign In</h3>
+        ) : (
+          <h3 className='login-form-title'>Create an Account</h3>
+        )}
         <form className='login-form' onSubmit={onSubmit}>
           <div className='input-wrapper-top'>
             <label className='login-username-label'>Username</label>
@@ -75,38 +88,68 @@ export default function LoginForm({id, setSelectedForm}) {
               name='password'
               onChange={(e) => setPassword(e.target.value)}
             />
-          </div>
+          </div>{' '}
           <div className='input-wrapper-bottom'>
-            <label className='login-password-label'>Email</label>
-            <input
-              className='password-input'
-              type='text'
-              id={`${loginFormId}-email`}
-              value={email}
-              name='email'
-              onChange={(e) => setEmail(e.target.value)}
-            />
+            {showLogin ? (
+              <>
+                <label className='login-email-label'>Email</label>
+                <input
+                  className='password-input'
+                  type='text'
+                  id={`${loginFormId}-email`}
+                  value={email}
+                  name='email'
+                  onChange={(e) => setEmail(e.target.value)}
+                />{' '}
+              </>
+            ) : null}
           </div>
           <div className='buttons'>
-            <div className='login-button-container'>
-              <Button
-                id={`${loginFormId}-submit`}
-                className='login-button'
-                type='submit'
-              >
-                Log In
-              </Button>
-            </div>
-            <div className='signup-button-container'>
-              <Button
-                id={`${loginFormId}-sign-up`}
-                className='signup-button'
-                type='button'
-                onClick={(e) => createUser(e.target.value)}
-              >
-                Sign Up
-              </Button>
-            </div>
+            {showLogin ? (
+              <>
+                <div className='login-button-container'>
+                  <Button
+                    id={`${loginFormId}-submit`}
+                    className='login-button'
+                    type='submit'
+                  >
+                    Submit
+                  </Button>
+                </div>
+                <div className='signup-button-container'>
+                  <Button
+                    id={`${loginFormId}-sign-up`}
+                    className='signup-button'
+                    type='button'
+                    onClick={() => setShowLogin(false)}
+                  >
+                    Sign Up
+                  </Button>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className='login-button-container'>
+                  <Button
+                    id={`${loginFormId}-submit`}
+                    className='login-button'
+                    type='submit'
+                  >
+                    Submit
+                  </Button>
+                </div>
+                <div className='signup-button-container'>
+                  <Button
+                    id={`${loginFormId}-sign-up`}
+                    className='signup-button'
+                    type='button'
+                    onClick={() => setShowLogin(true)}
+                  >
+                    Login
+                  </Button>
+                </div>
+              </>
+            )}
           </div>
         </form>
         <hr className='login-divider' />
