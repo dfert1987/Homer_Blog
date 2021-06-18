21b75f74e9f3a093da2d3362763ea7a8ee255118
David Fertitta <davidfertitta@Davids-MBP-2.lan>
2021-06-17 19:20:35 -0600

    add a post only conditionally renders for admin only

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import NavBar from './Components/NavBar/NavBar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import './App.css';
 import Home from './Components/Pages/Home';
 import About from './Components/Pages/About';
 import Bulls from './Components/Pages/Bulls';
@@ -11,22 +10,39 @@ import Meatball from './Components/Pages/Meatball';
 import Login from './Components/Pages/Login';
 import AdminCreatePost from './Admin/AdminCreatePost';
 import Blog from './Components/Pages/Blog';
+import './App.css';
 
 function App() {
+  const [userProfile, setUserProfile] = useState();
+
+  useEffect(() => {
+    setUserProfile(JSON.parse(localStorage.getItem('user')));
+  }, [setUserProfile]);
+
   return (
     <>
       <Router>
-        <NavBar />
+        <NavBar user={userProfile} />
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/about' exact component={About} />
-          <Route path='/login' exact component={Login} />
-          <Route path='/bulls' exact component={Bulls} />
-          <Route path='/bears' exact component={Bears} />
-          <Route path='/cubs' exact component={Cubs} />
-          <Route path='/meatball' exact component={Meatball} />
-          <Route path='/blog/:blogId' component={Blog} />
-          <Route path='/AdminCreatePost' exact component={AdminCreatePost} />
+          <Route path='/' exact component={Home} user={userProfile} />
+          <Route path='/about' exact component={About} user={userProfile} />
+          <Route path='/login' exact component={Login} user={userProfile} />
+          <Route path='/bulls' exact component={Bulls} user={userProfile} />
+          <Route path='/bears' exact component={Bears} user={userProfile} />
+          <Route path='/cubs' exact component={Cubs} user={userProfile} />
+          <Route
+            path='/meatball'
+            exact
+            component={Meatball}
+            user={userProfile}
+          />
+          <Route path='/blog/:blogId' component={Blog} user={userProfile} />
+          <Route
+            path='/AdminCreatePost'
+            exact
+            component={AdminCreatePost}
+            user={userProfile}
+          />
         </Switch>
       </Router>
     </>
