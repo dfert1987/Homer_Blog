e3f9d772703a911fa959fb05f865817abc24bc17
David Fertitta <davidfertitta@Davids-MacBook-Pro-2.local>
2021-06-23 19:07:20 -0600

    update username and password form now has basic css

--- src/Components/Pages/UserProfile/UpdatePass.js
+++ src/Components/Pages/UserProfile/UpdatePass.js
@@ -25,66 +25,72 @@ export default function UpdatePass({id, setToggle}) {
         </p>
       </div>
       <form onSubmit={onSubmit()} className='username-password-form'>
-        <div className='old-password-container'>
-          <label className='old-password-label'>Current Password:</label>
-          <input
-            className='old-password-input'
-            type='password'
-            id={`${id}-old-password`}
-            value={currentPassword}
-            name='currentPassword'
-            disabled={disabled}
-            onChange={(e) => setCurrentPassword(e.target.value)}
-          />
+        <div className='both-forms'>
+          <div className='old-password-container'>
+            <label className='old-password-label'>Current Password:</label>
+            <input
+              className='old-password-input'
+              type='password'
+              id={`${id}-old-password`}
+              value={currentPassword}
+              name='currentPassword'
+              disabled={disabled}
+              onChange={(e) => setCurrentPassword(e.target.value)}
+            />
+            <Button
+              className='submit-old'
+              type='button'
+              onClick={() => setDisabled(true)}
+            >
+              Submit
+            </Button>
+          </div>
+          <div className='new-pass-name-container'>
+            <div className='inputs-container'>
+              <div className='new-pass-container'>
+                <label className='new-pass-label'>New Password:</label>
+                <input
+                  className='new-pass-input'
+                  type='password'
+                  id={`${id}-new-password`}
+                  value={newPassword}
+                  name='newPassword'
+                  disabled={!disabled}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                />
+              </div>
+              <div className='new-username-container'>
+                <label className='new-username-label'>New Username:</label>
+                <input
+                  className='new-username-input'
+                  type='text'
+                  id={`${id}-new-username`}
+                  value={newUsername}
+                  name='newUsername'
+                  disabled={!disabled}
+                  onChange={(e) => setNewUsername(e.target.value)}
+                />
+              </div>
+            </div>
+              <div className='new-button'>
+                <Button type='submit'>Submit</Button>
+              </div>
+          </div>
+        </div>
+        <div className='main-button-container'>
           <Button
-            className='submit-old'
             type='button'
-            onClick={() => setDisabled(true)}
+            className='edit-info-button'
+            onClick={() => setToggle()}
           >
-            Submit
+            Edit User Info
           </Button>
-        </div>
-        <div className='new-pass-name-container'>
-          <div className='inputs-container'>
-            <div className='new-pass-container'>
-              <label className='new-pass-label'>New Password:</label>
-              <input
-                className='new-pass-input'
-                type='password'
-                id={`${id}-new-password`}
-                value={newPassword}
-                name='newPassword'
-                disabled={!disabled}
-                onChange={(e) => setNewPassword(e.target.value)}
-              />
-            </div>
-            <div className='new-username-container'>
-              <label className='new-username-label'>New Username:</label>
-              <input
-                className='new-username-input'
-                type='text'
-                id={`${id}-new-username`}
-                value={newUsername}
-                name='newUsername'
-                disabled={!disabled}
-                onChange={(e) => setNewUsername(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className='buttons-container'>
-            <div className='new-button'>
-              <Button type='submit'>Submit</Button>
-            </div>
-            <div className='exit-button'>
-              <Link to='/'>
-                <Button>Exit</Button>
-              </Link>
-            </div>
+          <div className='exit-button'>
+            <Link to='/'>
+              <Button>Exit</Button>
+            </Link>
           </div>
         </div>
-        <Button type='button' className='edit-info-button' onClick={() => setToggle()}>
-          Edit User Info
-        </Button>
       </form>
     </>
   );
